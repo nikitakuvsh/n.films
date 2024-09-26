82ee0a43b8d9f39a999844761a53ce5de7995c9d
nikitakuvsh <nikita_kuvshinnikov@inbox.ru>
2024-09-27 01:24:10 +0300

    fix-bugs-test-db

--- src/components/SearchFilms/SearchFilms.js
+++ src/components/SearchFilms/SearchFilms.js
@@ -24,7 +24,7 @@ const ModalPlayer = ({ videoUrl, onClose }) => {
           width="560"
           height="315"
           src={videoUrl}
-          title="YouTube video player"
+          title="videoPlayer"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
